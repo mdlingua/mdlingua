9d2e3053944257a3c037c3ebf2e3b7688c00930a
Arye Lukashevski <aryel@wix.com>
2015-11-26 14:23:28 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mdlinguaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mdlinguaApp
+ */
+angular.module('mdlinguaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
