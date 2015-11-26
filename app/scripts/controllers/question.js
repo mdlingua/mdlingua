a1c932ebe050c39c2853e39a3196517199cff556
Arye Lukashevski <aryel@wix.com>
2015-11-26 22:24:27 +0200

    questions design

--- app/scripts/controllers/question.js
+++ app/scripts/controllers/question.js
@@ -8,10 +8,15 @@
  * Controller of the mdlinguaApp
  */
 angular.module('mdlinguaApp')
-  .controller('QuestionCtrl', function (questionService) {
+  .controller('QuestionCtrl', function (questionService, tts, $filter) {
     this.questions = questionService.getQuestionsForSidebar();
     this.currentQuestion = questionService.getCurrentQuestion();
     this.choose = function(answer) {
       console.log(answer);
     };
+    this.read = function(msg, lang){
+      var chooseLang = $filter('chooseLang');
+      var msg2 = chooseLang(msg, lang);
+      tts.read(msg2, lang);
+    }
   });
